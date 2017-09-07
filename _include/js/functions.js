7b6f61d986a0ac07a62970b86a9aa30164ac9cda
Rustam Shakirov <shakirov@uchi.ru>
2017-09-07 14:45:32 +0400

    Any comment

---
+++ _include/js/functions.js
@@ -0,0 +1,6 @@
+
+  function getRandomPos() {
+    var left = Math.round(Math.random() * (240));
+    var top = Math.round(Math.random() * (240));
+    return{"left":left, "top":top};
+    }
--- _include/js/script.js
+++ _include/js/script.js
@@ -5,32 +5,24 @@ $(document).ready(function() {
   data.setka = data.scene.find(".setka");
   data.c = []
   data.s = []
-  //data.start = []
-  //data.direction = []
+
+
   var c = []
   for (var i = 0; i < 9; i++) {
     var b = $("<div>", {class: "img img" + i});
-    b.css({left: 0  , top: 0});
+    pos = getRandomPos();
+    b.css(pos)
     b.appendTo(data.box);
-    c.push(b);
     data.c.push(b);
-  //let direction = []
-    var left = Math.round(Math.random() * (240))  + 'px';
-    var top = Math.round(Math.random() * (240))  + 'px';
-    c[i].css({left: left, top: top});
-    //data.direction.push(left,top);
   }
-//c[0].css({left: 120})
+
   var s = []
   for (var i = 0; i < 9; i++) {
     var a = $("<div>").addClass("b b" + i).appendTo(data.setka)
-    s.push(a);
     data.s.push(a);
   }
-  //var start = []
+
   var button = {
     name: "Готово"
-    //data.start.push(button);
   };
-  console.log(data)
 });
