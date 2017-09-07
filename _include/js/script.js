9a1ba1ce7581dc095e13f0ea5f6cd7ee1f79c6c0
Rustam Shakirov <shakirov@uchi.ru>
2017-09-07 10:12:50 +0400

    Any comment

--- _include/js/script.js
+++ _include/js/script.js
@@ -1,34 +1,36 @@
 $(document).ready(function() {
-  // При загрузке страницы выполнится весь написанный здесь код
-  // alert("Привет!!!");
-
   var data = {};
   data.scene = $(".scene");
   data.box = data.scene.find(".box");
   data.setka = data.scene.find(".setka");
-  data.c = data.scene.find(".box");
-  data.s = data.scene.find(".setka");
-
-
+  data.c = []
+  data.s = []
+  //data.start = []
+  //data.direction = []
   var c = []
   for (var i = 0; i < 9; i++) {
     var b = $("<div>", {class: "img img" + i});
     b.css({left: 0  , top: 0});
     b.appendTo(data.box);
-    c.push(b)
+    c.push(b);
+    data.c.push(b);
+  //let direction = []
+    var left = Math.round(Math.random() * (240))  + 'px';
+    var top = Math.round(Math.random() * (240))  + 'px';
+    c[i].css({left: left, top: top});
+    //data.direction.push(left,top);
   }
-  /*c[0].css({left: 120})
-  c[1].css({left: 240})
-  c[2].css({left: 360}) */
-
-
-
-  //c[3].css({left: 400})
-  console.log(data)
-
+//c[0].css({left: 120})
   var s = []
   for (var i = 0; i < 9; i++) {
     var a = $("<div>").addClass("b b" + i).appendTo(data.setka)
-    s.push(b)
+    s.push(a);
+    data.s.push(a);
   }
+  //var start = []
+  var button = {
+    name: "Готово"
+    //data.start.push(button);
+  };
+  console.log(data)
 });
