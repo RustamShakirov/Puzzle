1d3284447f7d1eb51d9ca529b71653194ea13a2d
Rustam Shakirov <shakirov@uchi.ru>
2017-09-21 17:29:27 +0400

    Any comment

--- _include/js/functions.js
+++ _include/js/functions.js
@@ -4,3 +4,20 @@
     var top = 50 + Math.round(Math.random() * (240));
     return{"left":left, "top":top};
   }
+
+
+  function positionScene(new_pos, data) {
+    if (new_pos.left < 0) {
+      new_pos.left = 0;
+    }
+    else if (new_pos.left > data.scene.width() - 120) {
+      new_pos.left = data.scene.width() - 120;
+    }
+    if (new_pos.top < 0) {
+      new_pos.top = 0;
+    }
+    else if (new_pos.top > data.scene.height() - 120) {
+      new_pos.top = data.scene.height() - 120;
+    }
+    return new_pos;
+  }
--- _include/js/script.js
+++ _include/js/script.js
@@ -1,19 +1,11 @@
 $(document).ready(function() {
   var data = {};
-  data.scene = {
-    self: $(".scene"),
-    pos: {
-      leftMin: null,
-      leftMax: null,
-      topMin: null,
-      topMax: null
-    }
-  };
-  var _scene_offset = data.scene.self.offset();
-  data.box = data.scene.self.find(".box");
-  data.setka = data.scene.self.find(".setka");
+  data.scene = $(".scene");
+  data.box = data.scene.find(".box");
+  data.setka = data.scene.find(".setka");
   data.c = [];
   data.s = [];
+  data.body = $(".body")
 
   var c = [];
   for (var i = 0; i < 9; i++) {
@@ -23,49 +15,6 @@ $(document).ready(function() {
     b.appendTo(data.box);
     data.c.push(b);
   }
-
-  var elems = $(".box .img");
-  var parent = data.scene.self;
-  elems.on('mousedown', function(event) { //Событие нажатия
-    var elem = $(this);
-    var pos = {};
-    pos.inner = { //Позиция курсора относительно элемента
-      left: event.offsetX, top: event.offsetY
-    };
-    //$("body").on  //parent.on (Работает в пределах сцены).
-    $("body").on('mousemove', function(event) {  // запомнить позицию курсора относит элемента
-      pos.parent = parent.offset(); //позиция родителя относитешльно экрана
-
-      pos.cursor = { //Позиция курсора относительно экрана
-        left: event.pageX, top: event.pageY
-      };
-
-      new_pos = { //Новая позиция элемента
-        left: pos.cursor.left - pos.parent.left - pos.inner.left,
-        top: pos.cursor.top - pos.parent.top - pos.inner.top
-      }
-      if (new_pos.left < 0) { //Условие для позиционирования left && top
-        new_pos.left = 0;
-      }
-      else if (new_pos.left > data.scene.self.width() - 120) {
-        new_pos.left = data.scene.self.width() - 120;
-      }
-      if (new_pos.top < 0) {
-        new_pos.top = 0;
-      }
-      else if (new_pos.top > data.scene.self.height() - 120) {
-        new_pos.top = data.scene.self.height() - 120;
-      }
-      elem.css(new_pos);
-    });
-
-    $("body").on('mouseup', function(event) { //elems.on(был)
-    	var elem = $(this)
-      elem.off("mouseup")  //Снять события перетаскивания и отжатия мыши
-      $("body").off("mousemove")  //parent.off(был)
-    });
-  });
-
   var s = [];
   for (var i = 0; i < 9; i++) {
     var a = $("<div>").addClass("b b" + i).appendTo(data.setka)
@@ -75,5 +24,36 @@ $(document).ready(function() {
   var button = {
     name: "Готово"
   };
+  var body = $("body");
+  var elems = $(".box .img");
+  var parent = data.scene;
+  elems.on('mousedown', function(event) { //Событие нажатия
+    var elem = $(this);
+    var pos = {};
+    pos.inner = { //Позиция курсора относительно элемента
+      left: event.offsetX, top: event.offsetY
+    };
 
+    pos.parent = parent.offset(); //позиция родителя относитешльно экрана
+    body.on('mousemove', function(event) {  // запомнить позицию курсора относит элемента
+
+      pos.cursor = { //Позиция курсора относительно экрана
+        left: event.pageX, top: event.pageY
+      };
+
+      new_pos = { //Новая позиция элемента
+        left: pos.cursor.left - pos.parent.left - pos.inner.left,
+        top: pos.cursor.top - pos.parent.top - pos.inner.top
+      }
+      new_pos = positionScene(new_pos, data);  //Отдельная функция(для сцены)
+      elem.css(new_pos);
+    });
+    /*Если мышь отпустили за пределами сцены,
+    перетаскиваемый фрагмент должен отлететь на исходную позицию */
+    body.on('mouseup', function(event) {
+    	var elem = $(this)
+      elem.off("mouseup")  //Снять события перетаскивания и отжатия мыши
+      body.off("mousemove")
+    });
+  });
 });
